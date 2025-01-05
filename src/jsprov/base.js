5396163444a4b56171b091bdfe8bd20ef037d5c3
yiseoxpzuapa <unharmedc724@gmail.com>
2025-01-05 02:37:29 -0600

    Add files via upload

---
+++ src/jsprov/base.js
@@ -0,0 +1,18 @@
+// HEAVY WIP NOT FULLY DONE (ill fix this later)
+
+
+import playermodel from './playermod/model';
+
+class ServerPacket {
+    constructor(player, client) {
+        this.player = player;
+        this.client = client;
+        this.playermodel = playermodel
+    }
+
+    join() {
+        playermodel.join
+    }
+}
+
+export default player;  
