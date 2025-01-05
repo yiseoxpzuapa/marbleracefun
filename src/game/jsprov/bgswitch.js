3d0f3d78b893b8241f61d7dc72c0b881a647ab23
yiseoxpzuapa <unharmedc724@gmail.com>
2025-01-05 02:52:27 -0600

    Update bgswitch.js

--- src/game/jsprov/bgswitch.js
+++ src/game/jsprov/bgswitch.js
@@ -1,5 +1,5 @@
 var index = 0;
-var imageList = ['https://github.com/yiseoxpzuapa/marbleracefun/blob/main/img/bg1.png'];
+var imageList = ['https://github.com/yiseoxpzuapa/marbleracefun/blob/main/img/bg1.png', 'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-96EA00D6BF0A7357A10349485D8B31A8-Png/150/150/AvatarHeadshot/Webp/noFilter'];
 
 function changeBG() {
   index = index + 1;
