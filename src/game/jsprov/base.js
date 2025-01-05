99f968b8f61ee397dbb98cf6b6b1cc8de613469c
yiseoxpzuapa <unharmedc724@gmail.com>
2025-01-05 06:37:00 -0600

    the beginning of actually making the game
    (VERYYYY BUGGY! and base.js barely finished! and broken!)

--- src/game/jsprov/base.js
+++ src/game/jsprov/base.js
@@ -1,7 +1,29 @@
-loadSprite("player","https://github.com/yiseoxpzuapa/marbleracefun/blob/f008b5636374196f842f3c7cbab4b2fc4955af0e/img/marble1.png?raw=true")
 
-const player = add([
-    sprite("player")
-]
-    
-)
+// bug filled :/ ill fix this later
+
+// bg
+let main;
+let context;
+
+//class
+window.onload = function() {
+    main = document.getElementById("main");
+    context = main.getAttribute("2d")
+    main.Height = mainHeight;
+    main.Width = mainWidth;
+}
+
+//marble
+let marbleHeight = 50;
+let marbleWidth = 50;
+let marbleY = mainWidth/2 - marbleWidth
+let marbleX = mainHeight/2 - marbleHeight
+
+let marble = {
+    x : marbleX,
+    y : marbleY,
+    height : marbleHeight,
+    width : marbleWidth
+}
+
+// dealing with adding images later especially cause selection
