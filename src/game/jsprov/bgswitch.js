36189c638e5d81e859acc0baa04be9953e4f1077
yiseoxpzuapa <unharmedc724@gmail.com>
2025-01-05 03:59:02 -0600

    Update bgswitch.js

--- src/game/jsprov/bgswitch.js
+++ src/game/jsprov/bgswitch.js
@@ -1,9 +1,9 @@
 var index = 0;
-var imageList2 = ['https://github.com/yiseoxpzuapa/marbleracefun/blob/main/img/bg1.png', 'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-96EA00D6BF0A7357A10349485D8B31A8-Png/150/150/AvatarHeadshot/Webp/noFilter', 'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-79940A8D0F0AEA08B09133D9BD018C61-Png/150/150/AvatarHeadshot/Webp/noFilter'];
+var imageList3 = ['https://github.com/yiseoxpzuapa/marbleracefun/blob/main/img/bg1.png', 'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-96EA00D6BF0A7357A10349485D8B31A8-Png/150/150/AvatarHeadshot/Webp/noFilter', 'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-79940A8D0F0AEA08B09133D9BD018C61-Png/150/150/AvatarHeadshot/Webp/noFilter'];
 
 function changeBG() {
   index = index + 1;
-  if (index == imageList2.length) {
+  if (index == imageList3.length) {
      index = 0;
   }
   var image1 = document.getElementById("myBackground");
