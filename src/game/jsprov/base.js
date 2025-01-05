3e01facc4f02b1c8775bffb5e18b4b407dd5669d
yiseoxpzuapa <unharmedc724@gmail.com>
2025-01-05 06:18:58 -0600

    concept 1 for base.js
    rewrite

--- src/game/jsprov/base.js
+++ src/game/jsprov/base.js
@@ -1,16 +1,7 @@
-// HEAVY WIP NOT FULLY DONE (ill fix this later)
-// ok im bouta crash out i was in Inspect and realized that VSC accidentally imported the first line to ESC and i didnt realize
+loadSprite("player","https://github.com/yiseoxpzuapa/marbleracefun/blob/f008b5636374196f842f3c7cbab4b2fc4955af0e/img/marble1.png?raw=true")
 
-class Player {
-    constructor(player, client) {
-        this.player = player;
-        this.client = client;
-        this.playermodel = playermodel
-    }
-
-    join() {
-        playermodel.join
-    }
-}
-
-module.exports = player;
+const player = add([
+    sprite("player")
+]
+    
+)
